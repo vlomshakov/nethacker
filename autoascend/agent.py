6b821cb37f5ab30ebd15e66447bb6f28c79fa263
Lomshakov <lomshakov@2A2I-AIR-9JY5HX.local>
2026-09-24 15:27:17 +0300

    nethackers evolve win

--- autoascend/agent.py
+++ autoascend/agent.py
@@ -1416,8 +1416,10 @@ class Agent:
 
         items = [item for item in flatten_items(self.inventory.items) if item.is_unambiguous() and
                  item.category == nh.POTION_CLASS and item.object.name in ['healing', 'extra healing', 'full healing']]
+        # hypothesis: drinking a known healing potion below half health gives all
+        # roles more buffer to survive the next attack during an ongoing fight.
         if (
-                (self.blstats.hitpoints < 1 / 3 * self.blstats.max_hitpoints
+                (self.blstats.hitpoints < 1 / 2 * self.blstats.max_hitpoints
                  or self.blstats.hitpoints < 8) and items
         ):
             yield True
